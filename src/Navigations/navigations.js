7f4f729c57a397b72dc69be55cbb87713562df8b
ramijrock <ramijrock@gmail.com>
2024-08-10 13:01:17 +0530

    navigation change

--- src/Navigations/navigations.js
+++ src/Navigations/navigations.js
@@ -1,35 +1,33 @@
-import React, { useContext, useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import React, {useContext, useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {readData} from '../utils/Utils';
-import appContext from "../context/appContext";
-import AppNavigation from "./appNavigation";
-import AuthNavigation from "./authNavigation";
-
+import appContext from '../context/appContext';
+import AppNavigation from './appNavigation';
+import AuthNavigation from './authNavigation';
 
 const Navigation = () => {
-    const context = useContext(appContext)
+  const context = useContext(appContext);
 
-    useEffect(() => {
-            CheckUserData();
-    }, [])
+  useEffect(() => {
+    CheckUserData();
+  }, []);
 
-    const CheckUserData = async () => {
-        let userData = await readData('user_details');
-        context.setUserData(userData);
-    }
+  const CheckUserData = async () => {
+    let userData = await readData('user_details');
+    context.setUserData(userData);
+  };
 
-
-    return (
-        <NavigationContainer>
-            {
-                context.userData?.token == null ? (
-                    <AuthNavigation />
-                ) : context.userData?.vendor_info?.kyc_complete == "yes" ?  (
-                    <AppNavigation />
-                ) : <KycStack vendor_info={context.userData.vendor_info} />
-            }
-        </NavigationContainer>
-    )
-}
+  return (
+    <NavigationContainer>
+      {context.userData?.token == null ? (
+        <AuthNavigation />
+      ) : context.userData?.vendor_info?.kyc_complete == 'yes' ? (
+        <AppNavigation />
+      ) : (
+        <KycStack vendor_info={context.userData.vendor_info} />
+      )}
+    </NavigationContainer>
+  );
+};
 
 export default Navigation;
